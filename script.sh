642699c6c174c7c9ca4e8568754d5e3e3c1a3d62
Usman Samar <usmansamar.live@gmail.com>
2025-12-07 22:31:26 +0500

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash/ 
+echo Hello World
+echo I hope you are fine, If you are free than kindly let me know about this 
+software= "htop"
+echo "$software"
+sleep 2
+echo "This is a great thing which are we done"
+
